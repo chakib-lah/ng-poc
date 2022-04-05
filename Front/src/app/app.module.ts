584ecc68b1e2020b31f14c232711c446797e527a
LAHNAKI Chakib <chakib.lahnaki@sully-group.fr>
2022-04-05 11:54:21 +0200

    (feat)[3]:Add models, resolvers, and services folder

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MovieComponent } from './movie/movie.component';
+import { MovieComponent } from './movie/components/movie.component';
 import { MovieModule } from "./movie/movie.module";
 import { AdminModule } from "./admin/admin.module";
 import { CoreModule } from "./core/core.module";
--- Front/src/app/movie/movie.module.ts
+++ Front/src/app/movie/movie.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MovieRoutingModule } from './movie-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MovieRoutingModule
   ]
 })
 export class MovieModule { }
