be619e908eff41592a03dec102b0eb546e8a760b
LAHNAKI Chakib <chakib.lahnaki@sully-group.fr>
2022-04-13 18:19:33 +0200

    (feat)[33]:add refresh token

--- Api/config/bundles.php
+++ Api/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
 ];
