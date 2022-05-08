93714879b65fd4f9ed97baf47a664f9f6d57b900
LAHNAKI Chakib <chakib.lahnaki@sully-group.fr>
2022-05-08 13:24:17 +0200

    (feat)[5]: handle login error

--- Front/src/app/authentication/login/login.component.ts
+++ Front/src/app/authentication/login/login.component.ts
@@ -5,52 +5,56 @@ import { AuthenticationService } from '../services/authentication.service';
 import { GenericValidator } from '../../shared/validators/generic-validators';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
-
-  // Use with the generic validation message class
-  displayMessage: { [key: string]: string } = {};
-  private validationMessages: { [key: string]: { [key: string]: string } };
-  private genericValidator: GenericValidator;
-
-
-  constructor(
-      private router: Router,
-      private authService: AuthenticationService,
-      private fb: FormBuilder
-  ) {
-    this.validationMessages = {
-      username: {
-        required: 'Please enter your username.'
-      },
-      password: {
-        required: 'Please enter your password.'
-      }
-    };
-    this.genericValidator = new GenericValidator(this.validationMessages);
-  }
+    loginForm!: FormGroup;
+    errorMessage = '';
+    // Use with the generic validation message class
+    displayMessage: { [key: string]: string } = {};
+    private validationMessages: { [key: string]: { [key: string]: string } };
+    private genericValidator: GenericValidator;
 
 
-  ngOnInit(): void {
-    this.loginForm = this.fb.group({
-      username: ['', [Validators.required]],
-      password: ['', [Validators.required]]
-    });
-  }
+    constructor(
+        private router: Router,
+        private authService: AuthenticationService,
+        private fb: FormBuilder
+    ) {
+        this.validationMessages = {
+            username: {
+                required: 'Please enter your username.'
+            },
+            password: {
+                required: 'Please enter your password.'
+            }
+        };
+        this.genericValidator = new GenericValidator(this.validationMessages);
+    }
 
-  public login() {
-    let username = this.loginForm.get('username')?.value;
-    let password = this.loginForm.get('password')?.value;
-    // TODO handle login error
-    this.authService
-        .login(username, password)
-        .subscribe(() => console.log('next'),
-             msg => console.log(msg)
-        );
-  }
+
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            username: ['', [Validators.required]],
+            password: ['', [Validators.required]]
+        });
+    }
+
+    public login() {
+        let username = this.loginForm.get('username')?.value;
+        let password = this.loginForm.get('password')?.value;
+        this.authService
+            .login(username, password)
+            .subscribe(
+                () => this.router.navigateByUrl('/'),
+                err => {
+                    if (err.status === 401) {
+                        this.errorMessage = 'The username or password are wrong';
+                    }
+                }
+            );
+    }
 
 }
